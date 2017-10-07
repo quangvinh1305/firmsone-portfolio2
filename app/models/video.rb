db60db923b1fa17001dfbe4e8fd4237472571610
Ubuntu <ubuntu@ip-172-31-27-252.us-west-2.compute.internal>
2017-10-07 00:40:13 +0000

    fix model

---
+++ app/models/video.rb
@@ -0,0 +1,3 @@
+class Video < ApplicationRecord
+  belongs_to :blog
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171007001144) do
+ActiveRecord::Schema.define(version: 20171007003835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -101,8 +101,18 @@ ActiveRecord::Schema.define(version: 20171007001144) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  create_table "videos", force: :cascade do |t|
+    t.string   "url"
+    t.string   "title"
+    t.integer  "blog_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["blog_id"], name: "index_videos_on_blog_id", using: :btree
+  end
+
   add_foreign_key "blogs", "topics"
   add_foreign_key "comments", "blogs"
   add_foreign_key "comments", "users"
   add_foreign_key "technologies", "portfolios"
+  add_foreign_key "videos", "blogs"
 end
